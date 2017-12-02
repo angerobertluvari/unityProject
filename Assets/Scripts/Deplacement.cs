f46c1a862d90ea3208263ce8f0902e230d60d9bb
Ange Luvari <angeluvari@pc-58.home>
2017-12-02 12:50:58 +0100

    init commit

---
+++ Assets/Scripts/Deplacement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour {
+	private float Speed = 8f;
+	private Vector2 DirectionDeplacement = Vector2.zero;
+	private CharacterController Player;
+	private Animator Anim;
+
+	// Use this for initialization
+	void Start () {
+		Player = GetComponent<CharacterController>();
+		Anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		DirectionDeplacement.x = Input.GetAxisRaw("Horizontal");
+		DirectionDeplacement.y = Input.GetAxisRaw("Vertical");
+		DirectionDeplacement = transform.TransformDirection(DirectionDeplacement);
+
+		Player.Move (DirectionDeplacement * Speed * Time.deltaTime);
+
+		/*if (Player.velocity.magnitude >= 0) {
+			Anim.SetFloat ("SpeedVertical", Mathf.Abs (DirectionDeplacement.y));
+			Anim.SetFloat ("SpeedHorizontal", Mathf.Abs (DirectionDeplacement.x));
+		} */
+
+		//Lancement de l'animation par rapport au deplacement du personnage
+		if (DirectionDeplacement.x <= 0) 
+			Anim.SetFloat ("SpeedGauche", Mathf.Abs (DirectionDeplacement.x));
+		if (DirectionDeplacement.x >= 0)
+			Anim.SetFloat ("SpeedDroite", Mathf.Abs (DirectionDeplacement.x));
+		if (DirectionDeplacement.y <= 0) 
+			Anim.SetFloat ("SpeedBas", Mathf.Abs (DirectionDeplacement.y));
+		if (DirectionDeplacement.y >= 0)
+			Anim.SetFloat ("SpeedHaut", Mathf.Abs (DirectionDeplacement.y));
+		
+
+	}
+}
---
+++ Assets/Scripts/TirHaut.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TirHaut : MonoBehaviour {
+	public GameObject Bullet;
+	public int Force;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.UpArrow)) {
+			GameObject Balle = Instantiate (Bullet, transform.position, Quaternion.identity) as GameObject;
+			Balle.GetComponent<Rigidbody> ().velocity = Vector2.up * Force * Time.deltaTime;
+			Destroy (Bullet, 1f);
+
+		}
+			
+	}
+}
